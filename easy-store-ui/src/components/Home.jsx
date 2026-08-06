c572ce1a85f9474cb776ebbb91aad82079b97828
Sib11122 <Dsahej2004@outlook.com>
2026-08-06 20:46:03 +0530

    Home page Layout complete

--- easy-store-ui/src/components/Home.jsx
+++ easy-store-ui/src/components/Home.jsx
@@ -1,8 +1,17 @@
-import PageHeading from './PageHeading';
-export default function Home(){
-    return(
-        <div className="home-container">
-            <PageHeading/>
-        </div>
-    );
+import PageHeading from "./PageHeading";
+import ProductListings from "./ProductListings";
+import products from "../data/products";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <PageHeading title="Explore Eazy stickers !">
+        Discover our collection of high-quality, fun, and customizable
+        stickers. Whether you're decorating your laptop, water bottle, or
+        journal, we have the perfect design for you. Shop now and make your
+        belongings truly your own!
+      </PageHeading>
+      <ProductListings products={products} />
+    </div>
+  );
 }
--- easy-store-ui/src/components/PageHeading.jsx
+++ easy-store-ui/src/components/PageHeading.jsx
@@ -1,9 +1,10 @@
-import PageTitle from './PageTitle';
-PageTitle;
-export default function PageHeading() {
+import PageTitle from "./PageTitle";
+
+export default function PageHeading({ title, children }) {
   return (
-    <>
-      <PageTitle title="Explore Eazy stickers !"/>
-    </>
-  )
+    <div className="page-heading-container">
+      <PageTitle title={title} />
+      <p className="page-heading-paragraph">{children}</p>
+    </div>
+  );
 }
