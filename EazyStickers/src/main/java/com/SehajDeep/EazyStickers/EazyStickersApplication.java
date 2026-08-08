8c2e406544cdc9ecaf28a53434d101c2c2926375
Sib11122 <Dsahej2004@outlook.com>
2026-08-08 13:32:05 +0530

    backend work started

---
+++ EazyStickers/src/main/java/com/SehajDeep/EazyStickers/EazyStickersApplication.java
@@ -0,0 +1,13 @@
+package com.SehajDeep.EazyStickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyStickersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyStickersApplication.class, args);
+	}
+
+}
