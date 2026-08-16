6903bfc409bd38bf25771252eda0afccf7e21a27
Sib11122 <Dsahej2004@outlook.com>
2026-08-16 23:28:01 +0530

    some changes in the project

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,46 +1,11 @@
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import apiClient from "../api/apiClient";
-import { useEffect, useState } from "react";
-//Hooks
-export default function Home() {
-  const[products,setProducts] =useState([]);
-  const[loading,setLoading] =useState([]);
-  const[error,setError] =useState(null);
-  useEffect(()=>{
-    fetchProducts();
-  },[]);
+import { useLoaderData } from "react-router-dom";
 
-  const fetchProducts=async()=>{
-    try{
-      setLoading(true);
-      const response= await apiClient.get("/products");
-    setProducts(response.data);
-    }
-    catch(error){
-      setError(
-        error.response?.data?.message||
-        "Failed to fetch proucts. Please try again"
-      );
-    }
-    finally{
-      setLoading(false);
-    }
-  };
-  if(loading){
-    return(
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products..</span>
-      </div>
-    );
-  }
-  if(error){
-    return(
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl text-red-500">Error:{error}</span>    
-    </div>
-    );
-  }
+// Hooks
+export default function Home() {
+  const products = useLoaderData();
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
@@ -50,4 +15,16 @@ export default function Home() {
       <ProductListings products={products} />
     </div>
   );
+}
+
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get("/products"); // Axios GET Request
+    return response.data;
+  } catch (error) {
+    throw new Response(
+      error.message || "Failed to fetch products. Please try again.",
+      { status: error.status || 500 }
+    );
+  }
 }
--- eazystore-ui/src/main.jsx
+++ eazystore-ui/src/main.jsx
@@ -2,46 +2,32 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Cart from "./components/Cart.jsx";
 import Login from "./components/Login.jsx";
 import Home from "../src/components/Home"
 import ErrorPage from "./components/ErrorPage.jsx";
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement:<ErrorPage/>,
-    children: [
-      {
-        index: true,
-        element: <Home/>
-      },
-      {
-        path: "/home",
-        element: <Home  />
-      },
-      {
-        path: "/about",
-        element: <About />
-      },
-      {
-        path: "/contact",
-        element: <Contact />
-      },
-      {
-        path: "/cart",
-        element: <Cart />
-      },
-      {
-        path: "/login",
-        element: <Login />
-      }
-    ]
-  }
-])
+import { productsLoader } from "../src/components/Home";
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} loader={productsLoader}  />
+    <Route path="/home" element={<Home />} loader={productsLoader} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />}  />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/products/:productId" e />
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={appRouter} />
